6bce858ea49446fe038478a25b4619415733a3a7
Alex_Lu <1004026475@qq.com>
2022-11-30 19:27:59 +0800

    update config file

--- crud.config.js
+++ crud.config.js
@@ -1,70 +1,67 @@
+const path = require('path');
 
-const fields = ['id', 'username', 'phone', 'gender', 'email', 'address', 'birthday'];
-const queryFields = ['username', 'phone'];
 const config = {
-    entity: 'user',
-    needCodeSplitting: true,
-    fields: {
-        username: {
-            prop: String,
-            type: 'input',
-            label: "用户名",
-            editable: false,
-        },
-        gender: {
-            prop: String,
-            type: 'radio',
-            label: "性别",
-            editable: false,
-        },
-        phone: {
-            prop: String,
-            type: 'input',
-            label: "手机号",
-            editable: false,
-        },
-        email: {
-            prop: String,
-            type: 'input',
-            label: "电子邮箱",
-            editable: false,
-        },
-        address: {
-            prop: String,
-            type: 'input',
-            label: "用户名",
-            editable: false,
-        },
-        birthday: {
-            prop: String,
-            type: 'datetime',
-            label: "生日",
-            editable: false,
-        }
+  entity: "user",
+  type: 'element-plus',
+  output: path.resolve(__dirname,'dist'),
+  editable: true,
+  hasPagination: true,
+  fields: {
+    username: {
+      prop: String,
+      type: "input",
+      label: "用户名",
+      editable: false,
     },
-    hasPagination: false,
-    hasQuery: true,
-    editable: true,
-    queryFields: ['username', 'phone'],
-    operations: ['create','update', 'remove', 'detail'],
-    dependencies: [
-        {
-            name: 'store',
-            isNeed: true,
-            path: '@/stores',
-            aliasName: 'userStore',
-            actionTypes: {
-                isNeed: true,
-                path: '@/stores/action-types',
-            },
-
-        },
-        {
-            name: 'router',
-            isNeed: true,
-        }
-    ]
+    gender: {
+      prop: String,
+      type: "radio",
+      label: "性别",
+      editable: false,
+    },
+    phone: {
+      prop: String,
+      type: "input",
+      label: "手机号",
+      editable: false,
+    },
+    email: {
+      prop: String,
+      type: "input",
+      label: "电子邮箱",
+      editable: false,
+    },
+    address: {
+      prop: String,
+      type: "input",
+      label: "用户名",
+      editable: false,
+    },
+    birthday: {
+      prop: String,
+      type: "datetime",
+      label: "生日",
+      editable: false,
+    },
+  },
+  queryFields: ["username", "phone",'email'],
+  operations: ["create", "update", "remove", "detail"],
+  // dependencies: [
+  //   {
+  //     name: "store",
+  //     isNeed: true,
+  //     path: "@/stores",
+  //     aliasName: "userStore",
+  //     actionTypes: {
+  //       isNeed: true,
+  //       path: "@/stores/action-types",
+  //     },
+  //   },
+  //   {
+  //     name: "router",
+  //     isNeed: true,
+  //   },
+  // ],
 };
-
-
+// console.log('indexconfig.js',config.output)
 module.exports = config;
