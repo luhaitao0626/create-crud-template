24fd7c761d358c06cb1cd61600fb1f73edc47272
Alex_Lu <1004026475@qq.com>
2023-01-15 21:44:57 +0800

    chore: template to english, default.config.cjs: datetime=> datepicker

--- lib/template/element-plus/index.vue
+++ lib/template/element-plus/index.vue
@@ -1,19 +1,19 @@
 <template>
   <el-form :inline="true" :model="form" name="queryForm">
     <el-form-item>
-      <el-button type="primary" @click="search">查询</el-button>
-      <el-button type="primary" @click="reset">清空</el-button>
+      <el-button type="primary" @click="search">query</el-button>
+      <el-button type="primary" @click="reset">reset</el-button>
     </el-form-item>
   </el-form>
   <el-table ref="multipleTableRef" :border="true" :data="tableData" style="width: 100%" highlight-current-row>
     <el-table-column type="selection" width="55"> </el-table-column>
-    <!-- 用户定义：可编辑 -->
+    <!-- user difined：editable mode -->
     <el-table-column mode="editable" v-for="item in tableHeader" :key="item.prop" :prop="item.prop" :label="item.label">
       <template #default="scope">
-        <!-- 进入编辑模式：该行处于编辑模式并且该字段是可编辑的 -->
+        <!-- in edit mode：scope(row) is editable and field is editable -->
         <div v-if="scope.row.editable && item.editable">
           <template v-if="item.type === 'input'">
-            <el-input size="default" v-model="scope.row[item.prop]" :placeholder="`请输入${item.label}`"></el-input>
+            <el-input size="default" v-model="scope.row[item.prop]" :placeholder="`please input ${item.label}`"></el-input>
           </template>
           <template v-else-if="item.type === 'datepicker'">
             <el-date-picker v-model="scope.row[item.prop]" type="date" placeholder="Pick a day" size="default" format="YYYY/MM/DD" value-format="YYYY-MM-DD"></el-date-picker>
@@ -22,44 +22,44 @@
             <el-switch v-model="scope.row[item.prop]" size="default"></el-switch>
           </template>
         </div>
-        <!-- 非编辑模式(展示模式) -->
+        <!-- display mode -->
         <div v-else class="editable-row">
           <el-switch v-if="item.type==='switch'" v-model="scope.row[item.prop]" size="default" disabled></el-switch>
           <span v-else class="editable-row-span">{{ scope.row[item.prop] }}</span>
         </div>
       </template>
     </el-table-column>
-    <el-table-column mode="editable" label="操作">
+    <el-table-column mode="editable" label="operations">
       <template #default="scope">
         <el-button v-show="!scope.row.editable" type="success" size="small" @click="scope.row.editable = true">
-          编辑
+          edit
         </el-button>
         <el-button v-show="scope.row.editable" type="success" size="small" @click="handleEdit(scope.$index, scope.row)">
-          确定
+          confirm
         </el-button>
         <el-button v-show="!scope.row.editable" type="danger" size="small"
           @click="handleDelete(scope.$index, scope.row)">
-          删除
+          remove
         </el-button>
         <el-button v-show="!scope.row.editable" type="info" size="small" @click="handleDetail(scope.$index, scope.row)">
-          查看
+          detail
         </el-button>
       </template>
     </el-table-column>
-    <!-- 用户定义：不可编辑 -->
+    <!-- user defined: uneditable mode -->
     <el-table-column mode="ineditable" v-for="item in tableHeader" :key="item.prop" :prop="item.prop"
       :label="item.label">
       <template #default="scope">
         <span>{{ scope.row[item.prop] }}</span>
       </template>
     </el-table-column>
-    <el-table-column mode="ineditable" label="操作">
+    <el-table-column mode="ineditable" label="operations">
       <template #default="scope">
         <el-button type="danger" size="small" @click="handleDelete(scope.$index, scope.row)">
-          删除
+          remove
         </el-button>
         <el-button type="info" size="small" @click="handleDetail(scope.$index, scope.row)">
-          查看
+          detail
         </el-button>
       </template>
     </el-table-column>
