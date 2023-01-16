b460d0a8591eaee2c555f2ab898ceebae20665fb
Alex_Lu <1004026475@qq.com>
2023-01-16 16:34:45 +0800

    feat(template): create funcionality finished

--- lib/template/element-plus/index.ts
+++ lib/template/element-plus/index.ts
@@ -3,8 +3,12 @@ import { getEntitys, remove, update } from "./crud";
 import { total } from './pagination';
 import { params } from "./query";
 import { columns } from './columns';
+import { ElMessage } from 'element-plus'
 
 export let tableData: any = ref([]);
+
+// flag: identify is creating a new data now
+const isCreating = ref(false);
 
 // Table
 export const tableHeader = reactive(columns);
@@ -14,13 +18,48 @@ export const setEntitys = async () => {
     let entitys = await getEntitys(params.value);
     tableData.value = entitys.list;
     total.value = entitys.total;
+};
+
+// create a new line in table to operate
+export const create = async () => {
+    if (isCreating.value) return ElMessage({
+        message: 'Warning, you have already in creating mode. Please finish first',
+        type: 'warning',
+    })
+    isCreating.value = true;
+    const newData = {
+        name: "",
+        age: '',
+        active: '',
+        gender: "",
+        phone: "",
+        email: "",
+        address: "",
+        birthday: "",
+        editable: true,
+        isCreating: true,
+    }
+    tableData.value.unshift(newData)
 }
+
+// cancelCreate
+export const cancelCreate = async () => {
+    isCreating.value = false;
+    tableData.value.shift();
+}
+
+// create/confirm create
+export const handleCreate = async (index: any, row: any) => {
+    isCreating.value = false;
+    row.isCreating = undefined;
+    row.editable = false;
+};
 
 // edit/update
 export const handleEdit = async (index: any, row: any) => {
     let res = await update(row);
     row.editable = false;
-}
+};
 
 // delete
 export const handleDelete = async (index: any, row: any) => {
@@ -29,6 +68,4 @@ export const handleDelete = async (index: any, row: any) => {
 };
 
 // show detail
-export const handleDetail = (index: any, row: any) => {
-
-};
+export const handleDetail = (index: any, row: any) => { };
--- lib/template/element-plus/index.vue
+++ lib/template/element-plus/index.vue
@@ -1,94 +1,170 @@
 <template>
   <el-form :inline="true" :model="form" name="queryForm">
+    <el-form-item label="name">
+      <el-input
+        v-model="form.name"
+        placeholder="Please input entity"
+      ></el-input>
+    </el-form-item>
+    <el-form-item label="age">
+      <el-input v-model="form.age" placeholder="Please input age"></el-input>
+    </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="search">query</el-button>
       <el-button type="primary" @click="reset">reset</el-button>
+      <!-- NEED -->
+      <el-button type="success" @click="create">add</el-button>
     </el-form-item>
   </el-form>
-  <el-table ref="multipleTableRef" :border="true" :data="tableData" style="width: 100%" highlight-current-row>
-    <el-table-column type="selection" width="55"> </el-table-column>
+  <el-table
+    ref="multipleTableRef"
+    :border="true"
+    :data="tableData"
+    style="width: 100%"
+    highlight-current-row
+  >
     <!-- user difined：editable mode -->
-    <el-table-column mode="editable" v-for="item in tableHeader" :key="item.prop" :prop="item.prop" :label="item.label">
+    <el-table-column
+      mode="editable"
+      v-for="item in tableHeader"
+      :key="item.prop"
+      :prop="item.prop"
+      :label="item.label"
+    >
       <template #default="scope">
         <!-- in edit mode：scope(row) is editable and field is editable -->
         <div v-if="scope.row.editable && item.editable">
           <template v-if="item.type === 'input'">
-            <el-input size="default" v-model="scope.row[item.prop]" :placeholder="`please input ${item.label}`"></el-input>
+            <el-input
+              size="default"
+              v-model="scope.row[item.prop]"
+              :placeholder="`please input ${item.label}`"
+            ></el-input>
           </template>
           <template v-else-if="item.type === 'datepicker'">
-            <el-date-picker v-model="scope.row[item.prop]" type="date" placeholder="Pick a day" size="default" format="YYYY/MM/DD" value-format="YYYY-MM-DD"></el-date-picker>
+            <el-date-picker
+              v-model="scope.row[item.prop]"
+              type="date"
+              placeholder="Pick a day"
+              size="default"
+              format="YYYY/MM/DD"
+              value-format="YYYY-MM-DD"
+            ></el-date-picker>
           </template>
           <template v-else-if="item.type === 'switch'">
-            <el-switch v-model="scope.row[item.prop]" size="default"></el-switch>
+            <el-switch
+              v-model="scope.row[item.prop]"
+              size="default"
+            ></el-switch>
           </template>
         </div>
         <!-- display mode -->
         <div v-else class="editable-row">
-          <el-switch v-if="item.type==='switch'" v-model="scope.row[item.prop]" size="default" disabled></el-switch>
-          <span v-else class="editable-row-span">{{ scope.row[item.prop] }}</span>
+          <el-switch
+            v-if="item.type === 'switch'"
+            v-model="scope.row[item.prop]"
+            size="default"
+            disabled
+          ></el-switch>
+          <span v-else class="editable-row-span">{{
+            scope.row[item.prop]
+          }}</span>
         </div>
       </template>
     </el-table-column>
     <el-table-column mode="editable" label="operations">
       <template #default="scope">
-        <el-button v-show="!scope.row.editable" type="success" size="small" @click="scope.row.editable = true">
+        <el-button
+          v-show="!scope.row.editable"
+          type="success"
+          size="small"
+          @click="scope.row.editable = true"
+        >
           edit
         </el-button>
-        <el-button v-show="scope.row.editable" type="success" size="small" @click="handleEdit(scope.$index, scope.row)">
-          confirm
-        </el-button>
-        <el-button v-show="!scope.row.editable" type="danger" size="small"
-          @click="handleDelete(scope.$index, scope.row)">
+        <el-button
+          v-show="!scope.row.editable"
+          type="danger"
+          size="small"
+          @click="handleDelete(scope.$index, scope.row)"
+        >
           remove
         </el-button>
-        <el-button v-show="!scope.row.editable" type="info" size="small" @click="handleDetail(scope.$index, scope.row)">
+        <el-button
+          v-show="!scope.row.editable"
+          type="info"
+          size="small"
+          @click="handleDetail(scope.$index, scope.row)"
+        >
           detail
+        </el-button>
+        <el-button
+          v-show="scope.row.editable && scope.row.isCreating !== true"
+          type="success"
+          size="small"
+          @click="handleEdit(scope.$index, scope.row)"
+        >
+          confirm edit
+        </el-button>
+        <!-- NEED -->
+        <el-button
+          v-show="scope.row.editable && scope.row.isCreating === true"
+          type="success"
+          size="small"
+          @click="handleCreate(scope.$index, scope.row)"
+        >
+          confirm create
+        </el-button>
+        <el-button
+          v-show="scope.row.editable && scope.row.isCreating === true"
+          type="info"
+          size="small"
+          @click="cancelCreate(scope.$index, scope.row)"
+        >
+          cancel create
         </el-button>
       </template>
     </el-table-column>
     <!-- user defined: uneditable mode -->
-    <el-table-column mode="ineditable" v-for="item in tableHeader" :key="item.prop" :prop="item.prop"
-      :label="item.label">
-      <template #default="scope">
-        <span>{{ scope.row[item.prop] }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column mode="ineditable" label="operations">
-      <template #default="scope">
-        <el-button type="danger" size="small" @click="handleDelete(scope.$index, scope.row)">
-          remove
-        </el-button>
-        <el-button type="info" size="small" @click="handleDetail(scope.$index, scope.row)">
-          detail
-        </el-button>
-      </template>
-    </el-table-column>
   </el-table>
   <el-divider></el-divider>
-  <el-pagination v-model:current-page="pagination.pageNum" v-model:page-size="pagination.pageSize"
-    :page-sizes="[3, 5, 10, 20]" :small="false" :disabled="false" :background="true"
-    layout="total, sizes, prev, pager, next, jumper" :total="total" @size-change="handleSizeChange"
-    @current-change="handleCurrentChange" >
+  <el-pagination
+    v-model:current-page="pagination.pageNum"
+    v-model:page-size="pagination.pageSize"
+    :page-sizes="[3, 5, 10, 20]"
+    :small="false"
+    :disabled="false"
+    :background="true"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="total"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+  >
   </el-pagination>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from "vue";
-import { total, pagination, handleCurrentChange, handleSizeChange } from "./pagination";
 import {
-  form,
-  search,
-  reset
-} from "./query";
+  total,
+  pagination,
+  handleCurrentChange,
+  handleSizeChange,
+} from "./pagination";
+import { form, search, reset } from "./query";
 import {
   tableHeader,
   tableData,
   setEntitys,
+  create,
   handleEdit,
   handleDelete,
   handleDetail,
+  // NEED
+  handleCreate,
+  // NEED
+  cancelCreate
 } from ".";
-
 onMounted(() => {
   setEntitys();
 });
